755fa47b1ba8bed8e372d82bd4567ceb0e13747b
Daniel Salahi <dan.salahii@gmail.com>
2025-03-08 14:29:01 +0300

    Configure application startup with dynamic port, CORS, and request size limits

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule, ConfigValidationSchemaType } from './app.module';
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const configService = app.get(ConfigService<ConfigValidationSchemaType>);
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ extended: true, limit: '50mb' }));
+  app.enableCors();
+  await app.listen(configService.get<number>('PORT'));
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${configService.get<number>('PORT')}/`,
+  );
 }
 bootstrap();
